50a3958ddfd0ddbc4181fcb7aa73a58c13641156
manuelernesto <manuelernesto@outlook.com>
2021-03-09 19:57:59 +0100

    finish countdown timer

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -16,20 +16,34 @@
 package com.example.androiddevchallenge
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.androiddevchallenge.ui.theme.MyTheme
+import com.example.androiddevchallenge.ui.theme.rajdhani
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyTheme {
+            MyTheme(darkTheme = true) {
                 MyApp()
             }
         }
@@ -40,7 +54,7 @@ class MainActivity : AppCompatActivity() {
 @Composable
 fun MyApp() {
     Surface(color = MaterialTheme.colors.background) {
-        Text(text = "Ready... Set... GO!")
+        Home()
     }
 }
 
@@ -58,4 +72,181 @@ fun DarkPreview() {
     MyTheme(darkTheme = true) {
         MyApp()
     }
+}
+
+
+@Composable
+fun Home() {
+
+    val timer: Long by remember { mutableStateOf(60 * 1000) }
+    var tempTimer by remember { mutableStateOf(timer) }
+
+    var time by remember {
+        mutableStateOf(timeToString(timer))
+    }
+
+    var onStop by remember { mutableStateOf(false) }
+    var onPause by remember { mutableStateOf(false) }
+    var onStarted by remember { mutableStateOf(false) }
+
+    val countDownTimer = object : CountDownTimer(tempTimer, 1000) {
+        override fun onTick(msToFinished: Long) {
+            if (onPause) {
+                this.cancel()
+                tempTimer = msToFinished
+            }
+
+            time = timeToString(msToFinished)
+
+
+            if (onStop)
+                this.onFinish()
+        }
+
+        override fun onFinish() {
+            time = timeToString(timer)
+            tempTimer = timer
+            onStarted = false
+        }
+    }
+
+
+    Row(
+        modifier = Modifier
+            .fillMaxSize(),
+        horizontalArrangement = Arrangement.SpaceAround
+    ) {
+        CountDownLeft(time)
+
+        Column(
+            modifier = Modifier
+                .fillMaxHeight(),
+            verticalArrangement = Arrangement.SpaceAround,
+            horizontalAlignment = Alignment.End
+        ) {
+            Column(modifier = Modifier.rotate(90f)) {
+                Text(
+                    text = "COUNTDOWN", fontSize = 36.sp,
+                    fontFamily = rajdhani,
+                    fontWeight = FontWeight.Bold,
+                    color = colorResource(id = R.color.light_gray)
+                )
+                Text(
+                    text = "TIMER", fontSize = 36.sp,
+                    color = colorResource(id = R.color.dark_gray),
+                    fontFamily = rajdhani,
+                    fontWeight = FontWeight.Medium,
+                )
+            }
+
+            Column(modifier = Modifier.padding(end = 64.dp, top = 150.dp)) {
+                if (!onStarted) {
+                    Text(
+                        text = "PLAY",
+                        fontFamily = rajdhani,
+                        fontWeight = FontWeight.Medium,
+                        color = colorResource(id = R.color.dark_gray),
+                        fontSize = 20.sp,
+                        modifier = Modifier
+                            .padding(bottom = 16.dp)
+                            .clickable {
+                                countDownTimer.start()
+                                onStop = false
+                                onStarted = true
+                                onPause = false
+                            }
+                    )
+                } else {
+                    Text(
+
+                        text = "PAUSE",
+                        fontFamily = rajdhani,
+                        fontWeight = FontWeight.Medium,
+                        color = colorResource(id = R.color.dark_gray),
+                        fontSize = 20.sp,
+                        modifier = Modifier
+                            .padding(bottom = 16.dp)
+                            .clickable {
+                                onStarted = false
+                                onPause = true
+                            }
+                    )
+                }
+
+                Text(
+                    text = stringResource(R.string.stop_txt),
+                    color = colorResource(id = R.color.dark_gray),
+                    fontSize = 20.sp,
+                    modifier =
+                    Modifier
+                        .padding(bottom = 16.dp)
+                        .clickable {
+                            if (onPause)
+                                countDownTimer.onFinish()
+                            else {
+                                onPause = true
+                                onStop = true
+                            }
+                        }
+                )
+
+
+            }
+        }
+    }
+}
+
+@Composable
+private fun CountDownLeft(time: String) {
+    Column(
+        modifier = Modifier
+            .fillMaxHeight()
+            .padding(start = 52.dp),
+        verticalArrangement = Arrangement.SpaceAround
+    ) {
+
+        val hourInArray = time.split(",").toTypedArray()
+        val h = hourInArray[0]
+        val m = hourInArray[1]
+        val s = hourInArray[2]
+
+
+        TextTimer(timer = h, timerLabel = "H")
+        TextTimer(timer = m, timerLabel = "M")
+        TextTimer(timer = s, timerLabel = "S")
+    }
+}
+
+@Composable
+fun TextTimer(timer: String, timerLabel: String) {
+    Column {
+        Text(
+            text = timer,
+            fontSize = 100.sp,
+            fontFamily = rajdhani,
+            fontWeight = FontWeight.Bold,
+            color = colorResource(id = R.color.light_gray)
+        )
+        Box(
+            Modifier
+                .background(color = colorResource(id = R.color.dark_gray))
+                .size(width = 100.dp, height = 3.dp)
+        )
+        Text(
+            text = timerLabel,
+            fontSize = 40.sp,
+            color = colorResource(id = R.color.dark_gray),
+            fontFamily = rajdhani,
+            fontWeight = FontWeight.Medium,
+            modifier = Modifier.padding(start = 5.dp)
+        )
+
+    }
+}
+
+fun timeToString(time: Long): String {
+    val sec = (TimeUnit.MILLISECONDS.toSeconds(time) % 60).toString().padStart(2, '0')
+    val min = (TimeUnit.MILLISECONDS.toMinutes(time) % 60).toString().padStart(2, '0')
+    val hr = TimeUnit.MILLISECONDS.toHours(time).toString().padStart(2, '0')
+    return "${hr},${min},${sec}"
 }
---
+++ app/src/main/java/com/example/androiddevchallenge/ui/theme/MyFont.kt
@@ -0,0 +1,14 @@
+package com.example.androiddevchallenge.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.example.androiddevchallenge.R
+
+
+val rajdhani = FontFamily(
+    Font(R.font.rajdhani_light, FontWeight.Light),
+    Font(R.font.rajdhani_regular, FontWeight.Normal),
+    Font(R.font.rajdhani_medium, FontWeight.Medium),
+    Font(R.font.rajdhani_bold, FontWeight.Bold)
+)
